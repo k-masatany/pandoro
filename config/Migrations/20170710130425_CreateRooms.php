d5a49774f0984ed6676263efde229e1bf3a19478
k-masatany <masatani@fusic.co.jp>
2017-07-10 15:41:03 +0100

    [add] マイグレーションファイル追加。ルートディレクトリアクセス時ログインページに飛ばすように変更。

---
+++ config/Migrations/20170710130425_CreateRooms.php
@@ -0,0 +1,34 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreateRooms extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('rooms');
+        $table->addColumn('name', 'text', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('created', 'datetime', [
+            'default' => null,
+            'null' => true,
+        ]);
+        $table->addColumn('modified', 'datetime', [
+            'default' => null,
+            'null' => true,
+        ]);
+        $table->addColumn('deleted', 'datetime', [
+            'default' => null,
+            'null' => true,
+        ]);
+        $table->create();
+    }
+}
---
+++ config/Migrations/20170710130438_CreateRoomMembers.php
@@ -0,0 +1,42 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreateRoomMembers extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('room_members');
+        $table->addColumn('room_id', 'integer', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('user_id', 'integer', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('is_master', 'boolean', [
+            'default' => false,
+            'null' => false,
+        ]);
+        $table->addColumn('created', 'datetime', [
+            'default' => null,
+            'null' => true,
+        ]);
+        $table->addColumn('modified', 'datetime', [
+            'default' => null,
+            'null' => true,
+        ]);
+        $table->addColumn('deleted', 'datetime', [
+            'default' => null,
+            'null' => true,
+        ]);
+        $table->create();
+    }
+}
